e5937f53c6f7d20badfedc9e6932fabbe137b3a8
Igor-Palaguta <igor.palaguta@gmail.com>
2019-01-20 19:13:09 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "LeafProvider",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/leaf.git", majorVersion: 2),
+        .Package(url: "https://github.com/Igor-Palaguta/leaf.git", majorVersion: 2),
     ]
 )
